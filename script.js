bc0d9322d7d165ba6e844f3b87f09d99cd149496
Diego Plancarte <dplancarte7@gmail.com>
2021-01-11 09:57:50 -0800

    Changes quote api. Created conditional function to change size of quote and to account for unknown authors. Implemented event listeners for button clicks'

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quote from API
 async function getQuote() {
-  const proxyUrl = 'http://api.allorigins.win/get?url=';
-  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  const apiUrl = 'http://api.quotable.io/random';
   try {
-    const response = await fetch(proxyUrl + encodeURIComponent(apiUrl));
+    const response = await fetch(apiUrl);
     const data = await response.json();
-    const quoteObj = JSON.parse(data.contents);
-    console.log(quoteObj);
+    // If Author is blank, add 'Unknown'
+    if (data.author === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.author
+    }
+    // Reduce font size for long quotes
+    if (data.content.length > 120) {
+      quoteText.classList.add('long-quote')
+    } else {
+      quoteText.classList.remove('long-quote')
+    }
+    quoteText.innerText = data.content
   } catch (error) {
     getQuote();
-    console.log('whoops, no quote', error);
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote)
 
 // On Load
 getQuote();
