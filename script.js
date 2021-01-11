767bcec41e4afc7d2956dd01762e45be4be65664
Diego Plancarte <dplancarte7@gmail.com>
2021-01-11 10:04:49 -0800

    Created and implemented loading screen

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 // Get Quote from API
 async function getQuote() {
+  loading()
   const apiUrl = 'http://api.quotable.io/random';
   try {
     const response = await fetch(apiUrl);
@@ -23,6 +39,8 @@ async function getQuote() {
       quoteText.classList.remove('long-quote')
     }
     quoteText.innerText = data.content
+    // Stop loader, Show quote
+    complete();
   } catch (error) {
     getQuote();
   }
